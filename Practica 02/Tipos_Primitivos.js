0db35710b6bea1e0d57d60f53b6eaf09d1e7da1d
Maicol <mdmch13@gmail.com>
2024-10-09 23:45:14 -0300

    Se realizo la precticas 02 de javascript: Tipos de datos primitivos

---
+++ Practica 02/Tipos_Primitivos.js
@@ -0,0 +1,18 @@
+//TIPOS DE DATOS PRIMITIVOS
+
+//Number (Números)
+4;
+
+//String (Caracteres)
+"Hola Mundo...";
+`Hola Mundo...";`
+'Hola Mundo';
+
+//Boolean (Booleanos)
+true;
+false;
+
+//Null, Undefined (Nulo y valor indefinido)
+null;
+undefined;
+
