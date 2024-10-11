8c428818d6942b254b09f8ac9851605277625533
Maicol <mdmch13@gmail.com>
2024-10-10 22:06:29 -0300

    Se realiza la cuarta práctica sobre las notaciones en JS

---
+++ 04.Notación.js
@@ -0,0 +1,31 @@
+/*
+Notación en JS
+; -> sirve para delimitar el final de una línea
+. -> se utiliza en los objetos para acceder a los atributos
+[] -> se utiliza para las listas, arreglos o arrays
+{} -> se utiliza para las funciones, objetos y estructuras de control
+*/
+
+//;
+const b = 4;
+
+//.
+//móvil.anchura
+
+//[]
+const ar = [1,2,3,4];
+console.log(ar);
+
+//{}
+function suma(a,b){
+    //Se escribe la función
+}
+
+const móvil = {
+    anchura: 5,
+    altura: 10
+}
+
+if (true){
+    //Condición que hay entre llaves
+}
